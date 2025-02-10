bf00cd879fa90b807562bab179307d4bfb675175
Kousik Maity <113752271+Kousik1314@users.noreply.github.com>
2025-02-10 23:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (87.14%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string stack;
+        for(char ch: s){
+            if(isdigit(ch)){
+                if(!stack.empty()) stack.pop_back();
+            }
+            else{
+                stack.push_back(ch);
+            }
+        }
+        return stack;
+    }
+};
