6fae13a2358fc30ff02496c486085a18f4411ea3
Kousik Maity <113752271+Kousik1314@users.noreply.github.com>
2025-03-12 14:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (78.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // vector<int> ans;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[])
-        // }
         int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==0){
-                    nums[i]=nums[j];
-                    nums[j]=0;
-                }
-
+        int mover=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[mover];
+                nums[mover]=temp;
+                mover++;
             }
         }
-        
     }
 };
