b77842ca04cccbf2746786eb850ca2ef332d0dd5
Kousik Maity <113752271+Kousik1314@users.noreply.github.com>
2025-03-16 01:06:57 +0530

    Time: 17 ms (58.35%) | Memory: 26.2 MB (36.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        unordered_map<string, vector<string>> map;
-        int n=strs.size();
-        for(string str: strs){
-            string lexo=str;
-            sort(lexo.begin(),lexo.end());
-            if(map.find(lexo)!=map.end()){ //if mil gaya
-                map[lexo].push_back(str); //sorted wala string ke values par str push kar diya vector me
-            }
-            else{ //agar nhi mila
-                vector<string> v;
-                v.push_back(str);
-                map[lexo]=v;  //string ke value means vector ko daala, vector me str hai..
-            }
+        unordered_map<string, vector<string>> mp;
+        for(string x: strs){
+            string sorted=x;
+            sort(sorted.begin(),sorted.end());
+            mp[sorted].push_back(x);
         }
-        for(auto x: map){
-            vector<string> s=x.second;
-            ans.push_back(s);
+        vector<vector<string>> ans;
+        for(auto a: mp){
+            ans.push_back(a.second);
         }
         return ans;
     }
